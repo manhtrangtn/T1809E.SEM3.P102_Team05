7dfcbba1fdd3d35b238c8245c9bedeed76ecdb61
manhtrangtn <manhtrangtn@gmail.com>
2020-04-25 17:34:26 +0700

    first commit

---
+++ T1809E.SEM3.P102_Team05/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E.SEM3.P102_Team05.Models
+{
+    public class Product
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public double Price { get; set; }
+    }
+}
