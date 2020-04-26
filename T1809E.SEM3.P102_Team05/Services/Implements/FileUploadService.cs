b47511e7540c30a854295645e9a4fcfd5ac9840b
manhtrangtn <manhtrangtn@gmail.com>
2020-04-26 20:22:25 +0700

    trang created file upload service

---
+++ T1809E.SEM3.P102_Team05/Services/Implements/FileUploadService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace T1809E.SEM3.P102_Team05.Services.Implements
+{
+    public class FileUploadService
+    {
+      public string FileUpload(MultipartFileData file)
+      {
+        return null;
+      }
+    }
+}
