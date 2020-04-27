e7438dc99d0210e27f7aac78da24e99903cb8c13
TranBaSon <sontbth1808021@fpt.edu.vn>
2020-04-27 08:29:32 +0700

    add attributes to product class

--- T1809E.SEM3.P102_Team05/Models/Product.cs
+++ T1809E.SEM3.P102_Team05/Models/Product.cs
@@ -7,8 +7,24 @@ namespace T1809E.SEM3.P102_Team05.Models
 {
     public class Product
     {
-      public int Id { get; set; }
-      public string Name { get; set; }
-      public double Price { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Thumbnail { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public DateTime DeleteAt { get; set; }
+        public int InStock { get; set; }
+        public OrderStatus Status { get; set; }
+
+
+    }
+    enum OrderStatus
+    {
+        ACTIVE,
+        DISABLE,
+        OUT_OF_STOCK,
+        DELETED
     }
 }
